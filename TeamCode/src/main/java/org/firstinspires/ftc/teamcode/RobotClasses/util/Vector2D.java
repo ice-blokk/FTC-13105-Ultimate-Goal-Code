6901e996973fef62ab8a8e5bde7f4fc903e1b9dc
ice-blokk <62320136+ice-blokk@users.noreply.github.com>
2021-03-12 21:12:57 -0500

    Added rotate method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotClasses/util/Vector2D.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotClasses/util/Vector2D.java
@@ -24,4 +24,21 @@ public class Vector2D {
         return "X: " + x + "/ Y: " + y;
     }
 
+    /**
+     * Method taken from <a href = "https://github.com/wpilibsuite/allwpilib/blob/45590eea220923903606e47c7428939c3ce979e2/wpilibj/src/main/java/edu/wpi/first/wpilibj/drive/Vector2d.java#L33">
+     *     this doc</a> (WPI Lib).
+     *     <hr>
+     *         Rotate a vector in Cartesian space.
+     * @param angle angle in degrees by which to rotate vector counter-clockwise.
+     */
+    public void rotate(double angle) {
+        double cosA = Math.cos(angle * (Math.PI / 180.0));
+        double sinA = Math.sin(angle * (Math.PI / 180.0));
+        double[] out = new double[2];
+        out[0] = x * cosA - y * sinA;
+        out[1] = x * sinA + y * cosA;
+        x = out[0];
+        y = out[1];
+    }
+
 }
