9fe113288f02bc7fd5789eb061a526c7f43e97b3
ice-blokk <62320136+ice-blokk@users.noreply.github.com>
2021-03-20 22:52:22 -0400

    added goToPoint(), changed acceptable waypoint follow error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotClasses/LocalizationClasses/TrajectoryCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotClasses/LocalizationClasses/TrajectoryCommand.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.RobotClasses.LocalizationClasses;
 
 import org.firstinspires.ftc.teamcode.RobotClasses.Subsystems.MecanumDrive;
+import org.firstinspires.ftc.teamcode.RobotClasses.util.PIDController;
 import org.firstinspires.ftc.teamcode.RobotClasses.util.Pose2D;
 
 import java.util.ArrayList;
@@ -22,22 +23,49 @@ public class TrajectoryCommand {
     public boolean atX(Pose2D targetWaypoint) {
         double currentX = odometry.getCurrentPose().getX();
         double targetX = targetWaypoint.getX();
-        return currentX > targetX - .5 && currentX < targetX + .5;
+        return currentX > targetX - 1 && currentX < targetX + 1;
     }
 
     public boolean atY(Pose2D targetWaypoint) {
         double currentY = odometry.getCurrentPose().getY();
         double targetY = targetWaypoint.getY();
-        return currentY > targetY - .5 && currentY < targetY + .5;
+        return currentY > targetY - 1 && currentY < targetY + 1;
     }
 
     public boolean atRotation(Pose2D targetWaypoint) {
         double currentRotation = odometry.getCurrentPose().getRotation();
         double targetRotation = targetWaypoint.getRotation();
-        return currentRotation > targetRotation - 1 && currentRotation < targetRotation + 1;
+        return currentRotation > targetRotation - 3 && currentRotation < targetRotation + 3;
     }
 
-    public void followTrajectory() {
+    public void goToPoint(Pose2D targetWaypoint) {
+        PIDController pid = new PIDController(.01, .001, .01);
+        pid.setOutputRange(-.4, .4);
+        double forward, strafe, rotate;
+
+        if(!atX(targetWaypoint)) {
+            forward = -pid.calculate(odometry.getCurrentPose().getX(), targetWaypoint.getX());
+        }
+        else {
+            forward = 0;
+        }
+
+        if(!atY(targetWaypoint)) {
+            strafe = -pid.calculate(odometry.getCurrentPose().getY(), targetWaypoint.getY());
+        }
+        else {
+            strafe = 0;
+        }
+
+        if(!atRotation(targetWaypoint)) {
+            rotate = -pid.calculate(odometry.getCurrentPose().getRotation(), targetWaypoint.getRotation()) * .7;
+        }
+        else {
+            rotate = 0;
+        }
+
+        drivetrain.drive(forward, strafe, rotate);
+
 
     }
 
