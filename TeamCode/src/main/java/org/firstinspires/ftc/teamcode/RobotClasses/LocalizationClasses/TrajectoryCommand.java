6973acae5c5cfb00afa7108a94ca2b61041478bd
ice-blokk <62320136+ice-blokk@users.noreply.github.com>
2021-03-22 17:24:28 -0400

    changed constants, added power methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotClasses/LocalizationClasses/TrajectoryCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotClasses/LocalizationClasses/TrajectoryCommand.java
@@ -11,9 +11,13 @@ public class TrajectoryCommand {
     private Odometry odometry;
     private MecanumDrive drivetrain;
 
+    private double drivePower, rotatePower;
+
     public TrajectoryCommand(Odometry odometry, MecanumDrive drivetrain) {
         this.odometry = odometry;
         this.drivetrain = drivetrain;
+        drivePower = .4;
+        rotatePower = .4;
     }
 
     public void buildTrajectory(ArrayList<Pose2D> trajectory) {
@@ -23,48 +27,70 @@ public class TrajectoryCommand {
     public boolean atX(Pose2D targetWaypoint) {
         double currentX = odometry.getCurrentPose().getX();
         double targetX = targetWaypoint.getX();
-        return currentX > targetX - 1 && currentX < targetX + 1;
+        return currentX > targetX - 1.5 && currentX < targetX + 1.5;
     }
 
     public boolean atY(Pose2D targetWaypoint) {
         double currentY = odometry.getCurrentPose().getY();
         double targetY = targetWaypoint.getY();
-        return currentY > targetY - 1 && currentY < targetY + 1;
+        return currentY > targetY - 1.5 && currentY < targetY + 1.5;
     }
 
     public boolean atRotation(Pose2D targetWaypoint) {
         double currentRotation = odometry.getCurrentPose().getRotation();
         double targetRotation = targetWaypoint.getRotation();
-        return currentRotation > targetRotation - 3 && currentRotation < targetRotation + 3;
+        return currentRotation > targetRotation - 4 && currentRotation < targetRotation + 4;
+    }
+
+    public boolean atWaypoint(Pose2D targetWaypoint) {
+        return atX(targetWaypoint) && atY(targetWaypoint) && atRotation(targetWaypoint);
+    }
+
+    public void setDrivePower(double power) {
+        drivePower = power;
+    }
+
+    public void setRotatePower(double power) {
+        rotatePower = power;
     }
 
     public void goToPoint(Pose2D targetWaypoint) {
-        PIDController pid = new PIDController(.01, .001, .01);
-        pid.setOutputRange(-.4, .4);
+        PIDController forwardPID = new PIDController(.02, .001, .01);
+        PIDController strafePID = new PIDController(.02, .001, .01);
+        PIDController rotatePID = new PIDController(.01, 1, .001);
+
+        forwardPID.setOutputRange(-drivePower, drivePower);
+        strafePID.setOutputRange(-drivePower, drivePower);
+        rotatePID.setOutputRange(-rotatePower, rotatePower);
+
+        forwardPID.setDt(1);
+        strafePID.setDt(1);
+
         double forward, strafe, rotate;
 
         if(!atX(targetWaypoint)) {
-            forward = -pid.calculate(odometry.getCurrentPose().getX(), targetWaypoint.getX());
+            forward = -forwardPID.calculate(odometry.getCurrentPose().getX(), targetWaypoint.getX());
         }
         else {
             forward = 0;
         }
 
         if(!atY(targetWaypoint)) {
-            strafe = -pid.calculate(odometry.getCurrentPose().getY(), targetWaypoint.getY());
+            strafe = -strafePID.calculate(odometry.getCurrentPose().getY(), targetWaypoint.getY());
         }
         else {
             strafe = 0;
         }
 
         if(!atRotation(targetWaypoint)) {
-            rotate = -pid.calculate(odometry.getCurrentPose().getRotation(), targetWaypoint.getRotation()) * .7;
+            rotate = -rotatePID.calculate(odometry.getCurrentPose().getRotation(), targetWaypoint.getRotation()) * .6;
         }
         else {
             rotate = 0;
         }
 
         drivetrain.drive(forward, strafe, rotate);
+        odometry.update();
 
 
     }
